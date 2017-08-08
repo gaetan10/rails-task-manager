abca189af07ae7b428bbdb19aa49a8bf389e8f27
Gaetan May <gaetan.may@gmail.com>
2017-08-08 16:42:43 +0100

    exercise 1 done

--- app/controllers/manager_tasks_controller.rb
+++ app/controllers/manager_tasks_controller.rb
@@ -1,25 +1,55 @@
 class ManagerTasksController < ApplicationController
 
+  before_action :set_manager_task, only: [:show, :edit, :update, :destroy]
+
   def index
-    @task_list = ManagerTask.all
+    @manager_tasks = ManagerTask.all
   end
 
   def show
-    @task = ManagerTask.find(params[:id])
   end
 
   def new
+    @manager_task = ManagerTask.new
   end
 
   def create
+    @manager_task = ManagerTask.new(manager_task_params)
+    @manager_task.save
+
+    redirect_to manager_tasks_path(@manager_task)
+
   end
 
   def edit
   end
 
   def update
+    @manager_task.update(manager_task_params)
+
+    redirect_to manager_tasks_path(@manager_task)
+
   end
 
   def destroy
+    @manager_task.destroy
+
+    redirect_to manager_tasks_path(@manager_task)
+
+  end
+
+
+    private
+
+  def manager_task_params
+    # *Strong params*: You need to *whitelist* what can be updated by the user
+    # Never trust user data!
+    params.require(:manager_task).permit(:name, :description, :due_date)
+  end
+
+
+  def set_manager_task
+    @manager_task = ManagerTask.find(params[:id])
+
   end
 end
